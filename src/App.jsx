65761c2f37ec3d59b90d960a50c592d9a6e97652
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-09-23 14:57:51 -0500

    Fetch pokemon sprites and basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-	return <></>;
+	const [pokemon, setPokemon] = useState([]);
+	const [clicked, setClicked] = useState({});
+	const [highScore, setHighScore] = useState(0);
+
+	useEffect(() => {
+		let ignore = false;
+		let count = 1;
+
+		while (count <= 5) {
+			async function fetchPokemon() {
+				const random = Math.floor(Math.random() * 500);
+
+				const response = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${random}/`
+				);
+				const data = await response.json();
+
+				if (!ignore) {
+					setPokemon((pokemon) => [...pokemon, data]);
+				}
+			}
+
+			fetchPokemon();
+			count++;
+		}
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
+	return (
+		<div className=''>
+			<h1>Pokemon Memory Game</h1>
+			<p>
+				Score points for every <em>different</em> pokemon clicked
+			</p>
+			<ul className='flex gap-4 justify-center items-center '>
+				{pokemon.map((poke) => (
+					<li key={poke.id} className='bg-blue-300 rounded-lg'>
+						<img src={poke.sprites.front_default} alt={poke.name} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
